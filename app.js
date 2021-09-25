0bf02cb30fc26de2b0fcde7e395e45bdaf915da0
Arturo Quiroz <r2inlondon@gmail.com>
2021-09-25 21:35:47 +0100

    blue-cards file created and imported

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const nuestros = document.querySelector('.animate__animated');
 
-// function for animate.css
+// function to animate.css
 const observer = new IntersectionObserver(function(entries) {
 	if(entries[0].isIntersecting === true)
         nuestros.classList.add('animate__rubberBand');
