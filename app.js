d15a11a828f7ce6f9faccb1a90f922de8d0d9226
Arturo Quiroz <r2inlondon@gmail.com>
2021-09-26 19:59:53 +0100

    IntersectionObserver was not kicking in, this has been corrected

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const nuestros = document.querySelector('.nuestros-txt');
 
 // function to animate.css
-const observer = new IntersectionObserver(function(entries) {
-    console.log(entries);
-	if(entries[0].isIntersecting === true)
+const observer = new IntersectionObserver(function(entries) {    
+	if(entries[0].isIntersecting === true){
+        console.log('element is on viewport');
         // nuestros.classList.add('animate__rubberBand');
         entries[0].target.classList.add('animate__rubberBand');
+    }
+       
 }, { threshold: [1] });
 
 observer.observe(nuestros);
