fd57e99220110371851324bfb57c22f518de9715
Arturo Quiroz <r2inlondon@gmail.com>
2021-09-26 22:06:56 +0100

    animation for sections text has been added

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const nuestros = document.querySelector('.nuestros-txt');
+const bannerMsg = document.querySelector('.banner-msg'),
+    nuestrosTxt = document.querySelector('.nuestros'),
+    industrias = document.querySelector('.industrias');
+
 
 // function to animate.css
-const observer = new IntersectionObserver(function(entries) {    
-	if(entries[0].isIntersecting === true){
-        console.log('element is on viewport');
-        // nuestros.classList.add('animate__rubberBand');
-        entries[0].target.classList.add('animate__rubberBand');
-    }
-       
-}, { threshold: [1] });
+const observer = new IntersectionObserver(entries => { 
+    entries.forEach(element => {
+        if(element.isIntersecting === true){
+            // nuestros.classList.add('animate__rubberBand');
+            element.target.style.opacity = 1;
+            element.target.classList.add('animate__bounceInLeft');
+        }
+    });
 
-observer.observe(nuestros);
+       
+}, { threshold: [1] } );
+
+observer.observe(bannerMsg);
+observer.observe(nuestrosTxt);
+observer.observe(industrias);
+
+	// if(entries[0].isIntersecting === true){
+    //     // nuestros.classList.add('animate__rubberBand');
+    //     entries[0].target.style.opacity = 1;
+    //     entries[0].target.classList.add('animate__bounceInLeft');
+    // }
+
