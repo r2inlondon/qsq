a2b65150547324a1e420a7263c03befdbede2863
Arturo Quiroz <r2inlondon@gmail.com>
2022-03-29 22:58:55 +0100

    arrows hides after scroll

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const bannerMsg = document.querySelector('.banner-msg'),
-    nuestrosTxt = document.querySelector('.nuestros'),
-    industrias = document.querySelector('.industrias');
-
+const bannerMsg = document.querySelector(".banner-msg"),
+  nuestrosTxt = document.querySelector(".nuestros"),
+  industrias = document.querySelector(".industrias"),
+  arrows = document.querySelector(".arrow");
 
 // function to animate.css
-const observer = new IntersectionObserver(entries => { 
-    entries.forEach(element => {
-        if(element.isIntersecting === true){
-            // nuestros.classList.add('animate__rubberBand');
-            element.target.style.opacity = 1;
-            element.target.classList.add('animate__bounceInLeft');
-        }
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((element) => {
+      if (element.isIntersecting === true) {
+        // nuestros.classList.add('animate__rubberBand');
+        element.target.style.opacity = 1;
+        element.target.classList.add("animate__bounceInLeft");
+      }
     });
-
-       
-}, { threshold: [0.8] } );
+  },
+  { threshold: [0.8] }
+);
 
 observer.observe(bannerMsg);
 observer.observe(nuestrosTxt);
 observer.observe(industrias);
 
-	// if(entries[0].isIntersecting === true){
-    //     // nuestros.classList.add('animate__rubberBand');
-    //     entries[0].target.style.opacity = 1;
-    //     entries[0].target.classList.add('animate__bounceInLeft');
-    // }
-
+window.addEventListener("scroll", () => {
+  const y = window.scrollY;
+  if (y > 50) {
+    arrows.style.display = "none";
+  }
+});
