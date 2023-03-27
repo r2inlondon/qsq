a948014e6fe23984d4a227a283731da970f8c2f1
Arturo Quiroz <r2inlondon@gmail.com>
2023-03-27 11:34:57 +0100

    feat : removeLoader function implemented

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const bannerMsg = document.querySelector(".banner-msg"),
   nuestrosTxt = document.querySelector(".nuestros"),
   industrias = document.querySelector(".industrias"),
-  arrows = document.querySelector(".arrow");
+  arrows = document.querySelector(".arrow"),
+  loaderPage = document.querySelector(".loader-page"),
+  loaderTxt = document.querySelector(".loader-txt"),
+  pageWrapper = document.querySelector(".wrapper");
+
+const removeLoader = () => {
+  loaderPage.classList.add("fadeOut");
+  loaderTxt.classList.add("fadeOut");
+
+  loaderPage.addEventListener("animationend", () => {
+    loaderTxt.style.display = "none";
+    loaderPage.style.display = "none";
+    pageWrapper.style.display = "inline";
+  });
+};
+
+window.onload = () => {
+  removeLoader();
+};
 
 // function to animate.css
 const observer = new IntersectionObserver(
